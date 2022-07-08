eb2ffabeb0449eda2e602aeb11c026dede628bad
Lucas Mofardini <lucasmofardini@gmail.com>
2022-07-08 00:09:42 -0300

    Importei a font para o pasta public e tirei da src

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './fonts/google-sans/ProductSans-Regular.woff'
-import '@fortawesome/react-fontawesome';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

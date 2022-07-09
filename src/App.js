69b5a6e04a865a431ea5c973a844b49c1d3ce5c5
Lucas Mofardini <lucasmofardini@gmail.com>
2022-07-09 00:51:03 -0300

    Loading do card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.scss';
 import CardClima from './Component/CardClima/CardClima';
 import InputClima from './Component/InputClima/InputClima';
 
 function App() {
-  const [propriedadesCard, setPropriedadesCard] = useState(null);
   const [inputTempo, setInputTempo] = useState("");
-
-  // useEffect(()=>{
-  //   fetch('https://api.openweathermap.org/data/2.5/weather?id=3448433&appid=a8f8ddee03250606bf203bacf672de48&lang=pt_br&units=metric')
-  //   .then((res)=>{
-  //     if(res.ok){
-  //       return res.json();
-  //     }
-  //     throw new Error('Erro ao fazer a requisição' , res);
-  //   })
-  //   .then((json)=>{
-  //     setPropriedadesCard(json);
-  //   })
-  // },[]);
 
   return (
   <main>
@@ -26,7 +12,7 @@ function App() {
       <h1>Previsão do tempo</h1>
       <section className="section-input">
         <InputClima setValue={setInputTempo}/>
-        <p>{inputTempo}</p>
+        {/* <p>{inputTempo}</p> */}
       </section>
       <div className='container-cards'>
         <div className='box-card'>
@@ -34,6 +20,9 @@ function App() {
         </div>
         <div className='box-card'>
             <CardClima url={`https://api.openweathermap.org/data/2.5/weather?id=3410315&appid=a8f8ddee03250606bf203bacf672de48&lang=pt_br&units=metric`} />
+        </div>
+        <div className='box-card'>
+            <CardClima url={`https://api.openweathermap.org/data/2.5/weather?id=3470127&appid=a8f8ddee03250606bf203bacf672de48&lang=pt_br&units=metric`} />
         </div>
       </div>
     </div>
--- src/Component/CardClima/CardClima.jsx
+++ src/Component/CardClima/CardClima.jsx
@@ -1,15 +1,15 @@
 import React , {useEffect, useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import  ImageGif  from '../../Assets/images/load-gif.gif';
 
 import './card.scss';
 
 const CardClima = ({ url }) => {
-    const [infoTempo, setInfoTempo] = useState({});
-    // const [propriedades, setPropriedades] = useState(null);
+    const [infoTempo, setInfoTempo] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
-        // console.log(url);
       fetch(`${url}`)
       .then((res)=>{
         if(res.ok){
@@ -31,36 +31,46 @@ const CardClima = ({ url }) => {
             umidade: json.main.humidity,
             nuvens: json.clouds.all
         });
+        setLoading(false);
       })
-    },[]);
+    },[url]);
 
     return(
         <>
-        {infoTempo &&
-            <div className="container-card">     
-                <div className="box-1">
-                    <img src={`https://openweathermap.org/img/wn/${infoTempo.image_icon}@2x.png`} alt="Resposta Clima"></img>
-                </div>       
-                <div className="box-2">
-                    <div className="item-info__graus">
-                        <h1>{infoTempo.temp_atual} °C</h1>    
-                        <div className="item-info__minmax">
-                            <p className="maxima"><FontAwesomeIcon icon={faAngleUp}  /><b>{infoTempo.temp_maxima} °C</b></p>
-                            <p className="minima"><FontAwesomeIcon icon={faAngleDown}  /><b>{infoTempo.temp_minima} °C</b></p>   
-                        </div>             
+
+            <div className="container-card">   
+            {infoTempo &&  
+                <>
+                    <div className="box-1">
+                        <img src={`https://openweathermap.org/img/wn/${infoTempo.image_icon}@2x.png`} alt="Resposta Clima"/>
+                    </div>       
+                    <div className="box-2">
+                        <div className="item-info__graus">
+                            <h1>{infoTempo.temp_atual} °C</h1>    
+                            <div className="item-info__minmax">
+                                <p className="maxima"><FontAwesomeIcon icon={faAngleUp}  /><b>{infoTempo.temp_maxima} °C</b></p>
+                                <p className="minima"><FontAwesomeIcon icon={faAngleDown}  /><b>{infoTempo.temp_minima} °C</b></p>   
+                            </div>             
+                        </div>
+                        <div className="item-info__tempo">
+                            <p><small>Nuvens: </small><b>{infoTempo.nuvens}% </b></p>
+                            <p><small>Umidade: </small> <b>{infoTempo.umidade}% </b></p>
+                            <p><small>Sensação Térmica: </small> <b>{infoTempo.sensacao_termica} °C</b></p>
+                        </div>
+                    </div> 
+                    <div className="box-3">
+                            <h2>{infoTempo.nome_cidade}</h2>        
+                            <div><p>{infoTempo.descricao_cidade}</p></div>
+                    </div> 
+                </>
+                } 
+                {loading && 
+                    <div className="container-loading">
+                        <img width="100px" src={ImageGif} alt="Imagem de loading"/>
                     </div>
-                    <div className="item-info__tempo">
-                        <p><small>Nuvens: </small><b>{infoTempo.nuvens}% </b></p>
-                        <p><small>Umidade: </small> <b>{infoTempo.umidade}% </b></p>
-                        <p><small>Sensação Térmica: </small> <b>{infoTempo.sensacao_termica} °C</b></p>
-                    </div>
-                </div> 
-                <div className="box-3">
-                        <h2>{infoTempo.nome_cidade}</h2>        
-                        <div><p>{infoTempo.descricao_cidade}</p></div>
-                </div>     
+                }
             </div>
-        }
+     
         </>
     );
 }

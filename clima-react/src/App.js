14b58f0b4ae4be214000ebdff38de462263d200e
Lucas Mofardini <lucasmofardini@gmail.com>
2022-07-10 20:10:23 -0300

    Comecei o servidor node e coloquei o proxy no react

--- clima-react/src/App.js
+++ clima-react/src/App.js
@@ -12,18 +12,18 @@ function App() {
       <h1>Previsão do tempo</h1>
       <section className="section-input">
         <InputClima setValue={setInputTempo}/>
-        {/* <p>{inputTempo}</p> */}
+        <p>{inputTempo}</p>
       </section>
       <div className='container-cards'>
         <div className='box-card'>
             <CardClima url={`https://api.openweathermap.org/data/2.5/weather?id=3448433&appid=a8f8ddee03250606bf203bacf672de48&lang=pt_br&units=metric`} />
         </div>
-        <div className='box-card'>
+        {/* <div className='box-card'>
             <CardClima url={`https://api.openweathermap.org/data/2.5/weather?id=3410315&appid=a8f8ddee03250606bf203bacf672de48&lang=pt_br&units=metric`} />
         </div>
         <div className='box-card'>
             <CardClima url={`https://api.openweathermap.org/data/2.5/weather?id=3470127&appid=a8f8ddee03250606bf203bacf672de48&lang=pt_br&units=metric`} />
-        </div>
+        </div> */}
       </div>
     </div>
   </main>
